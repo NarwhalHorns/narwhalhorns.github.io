0fe10eaefd5b655e52a987bd38ead118c5dc996c
NarwhalHorns <jamestmason1997@gmail.com>
2019-05-24 00:43:34 +0100

    nm then

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 document.getElementById('copy').innerHTML = 'James Mason &copy; ' + new Date().getFullYear();
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('./sw.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
