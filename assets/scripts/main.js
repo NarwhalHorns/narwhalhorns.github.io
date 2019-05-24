8e61cce7cdc6b6e88004d51860a2add50924bf6c
NarwhalHorns <jamestmason1997@gmail.com>
2019-05-24 23:40:09 +0100

    some styling?

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,4 @@
-// Vue.component('my-footer', {
-//   template:
-//     `<footer>
-//       <ul>
-//         <li><a href="mailto:jamestmason1997@gmail.com">Email</a></li>
-//         <li><a href="https://www.instagram.com/justjamesmason">Instagram</a></li>
-//         <li><a href="https://twitter.com/jamjarmason">Twitter</a></li>
-//         <li><a href="https://github.com/NarwhalHorns">Github</a></li>
-//       </ul>
-//       <p id="copy"></p>
-//     </footer>`
-// })
-//
-// var app = new Vue({
-//   el: '#app'
-// })
-
-document.getElementById('copy').innerHTML = 'James Mason &copy; ' + new Date().getFullYear()
+document.getElementById('copy').innerHTML = 'Copyright &copy; ' + new Date().getFullYear() + ' James Mason'
 
 // if ('serviceWorker' in navigator) {
 //   window.addEventListener('load', function() {
